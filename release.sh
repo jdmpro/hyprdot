365b5c8b1d32553695a0967b4bfd1e9d8483523d
jdmpro <58115966+jdmpro@users.noreply.github.com>
2024-05-05 03:55:30 -0700

    Update release.sh

--- release.sh
+++ release.sh
@@ -10,36 +10,36 @@ ORANGE=$(tput setaf 166)
 YELLOW=$(tput setaf 3)
 RESET=$(tput sgr0)
 
-printf "${NOTE} Downloading / Checking for existing Hyprland-Dots.tar.gz...\n"
+printf "${NOTE} Downloading / Checking for existing hyprdot.tar.gz...\n"
 
 # Check if Hyprland-Dots.tar.gz exists
-if [ -f Hyprland-Dots.tar.gz ]; then
-  printf "${NOTE} Hyprland-Dots.tar.gz found.\n"
+if [ -f hyprdot.tar.gz ]; then
+  printf "${NOTE} hyprdot.tar.gz found.\n"
 
   # Get the version from the existing tarball filename
-  existing_version=$(echo Hyprland-Dots.tar.gz | grep -oP 'v\d+\.\d+\.\d+' | sed 's/v//')
+  existing_version=$(echo hyprdot.tar.gz | grep -oP 'v\d+\.\d+\.\d+' | sed 's/v//')
 
   # Fetch the tag_name for the latest release using the GitHub API
-  latest_version=$(curl -s https://api.github.com/repos/JaKooLit/Hyprland-Dots/releases/latest | grep "tag_name" | cut -d '"' -f 4 | sed 's/v//')
+  latest_version=$(curl -s https://api.github.com/repos/jdmpro/hyprdot/releases/latest | grep "tag_name" | cut -d '"' -f 4 | sed 's/v//')
 
   # Check if versions match
   if [ "$existing_version" = "$latest_version" ]; then
-    echo -e "${OK} Hyprland-Dots.tar.gz is up-to-date with the latest release ($latest_version)."
+    echo -e "${OK} hyprdot.tar.gz is up-to-date with the latest release ($latest_version)."
     
     # Sleep for 10 seconds before exiting
     printf "${NOTE} No update found. Sleeping for 10 seconds...\n"
     sleep 10
     exit 0
   else
-    echo -e "${WARN} Hyprland-Dots.tar.gz is outdated (Existing version: $existing_version, Latest version: $latest_version)."
+    echo -e "${WARN} hyprdot.tar.gz is outdated (Existing version: $existing_version, Latest version: $latest_version)."
     read -p "Do you want to upgrade to the latest version? (y/n): " upgrade_choice
     if [ "$upgrade_choice" = "y" ]; then
 		echo -e "${NOTE} Proceeding to download the latest release."
 		
 		# Delete existing directories starting with JaKooLit-Hyprland-Dots
-      find . -type d -name 'JaKooLit-Hyprland-Dots*' -exec rm -rf {} +
-      rm -f Hyprland-Dots.tar.gz
-      printf "${WARN} Removed existing Hyprland-Dots.tar.gz.\n"
+      find . -type d -name 'jdmpro-hyprdot*' -exec rm -rf {} +
+      rm -f hyprdot.tar.gz
+      printf "${WARN} Removed existing hyprdot.tar.gz.\n"
     else
       echo -e "${NOTE} User chose not to upgrade. Exiting..."
       exit 0
@@ -50,7 +50,7 @@ fi
 printf "${NOTE} Downloading the latest Hyprland source code release...\n"
 
 # Fetch the tag name for the latest release using the GitHub API
-latest_tag=$(curl -s https://api.github.com/repos/JaKooLit/Hyprland-Dots/releases/latest | grep "tag_name" | cut -d '"' -f 4)
+latest_tag=$(curl -s https://api.github.com/repos/jdmpro/hyprdot/releases/latest | grep "tag_name" | cut -d '"' -f 4)
 
 # Check if the tag is obtained successfully
 if [ -z "$latest_tag" ]; then
@@ -59,7 +59,7 @@ if [ -z "$latest_tag" ]; then
 fi
 
 # Fetch the tarball URL for the latest release using the GitHub API
-latest_tarball_url=$(curl -s https://api.github.com/repos/JaKooLit/Hyprland-Dots/releases/latest | grep "tarball_url" | cut -d '"' -f 4)
+latest_tarball_url=$(curl -s https://api.github.com/repos/jdmpro/hyprdot/releases/latest | grep "tarball_url" | cut -d '"' -f 4)
 
 # Check if the URL is obtained successfully
 if [ -z "$latest_tarball_url" ]; then
@@ -68,7 +68,7 @@ if [ -z "$latest_tarball_url" ]; then
 fi
 
 # Get the filename from the URL and include the tag name in the file name
-file_name="Hyprland-Dots-${latest_tag}.tar.gz"
+file_name="hyprdot-${latest_tag}.tar.gz"
 
 # Download the latest release source code tarball to the current directory
 if curl -L "$latest_tarball_url" -o "$file_name"; then
@@ -76,15 +76,15 @@ if curl -L "$latest_tarball_url" -o "$file_name"; then
   tar -xzf "$file_name" || exit 1
 
   # delete existing Hyprland-Dots
-  rm -rf JaKooLit-Hyprland-Dots
+  rm -rf jdmpro-hyprdot
 
   # Identify the extracted directory
   extracted_directory=$(tar -tf "$file_name" | grep -o '^[^/]\+' | uniq)
 
-  # Rename the extracted directory to JaKooLit-Hyprland-Dots
-  mv "$extracted_directory" JaKooLit-Hyprland-Dots || exit 1
+  # Rename the extracted directory to jdmpro-hyprdot
+  mv "$extracted_directory" jdmpro-hyprdot || exit 1
 
-  cd "JaKooLit-Hyprland-Dots" || exit 1
+  cd "jdmpro-hyprdot" || exit 1
 
   # Set execute permission for copy.sh and execute it
   chmod +x copy.sh
