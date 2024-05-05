a2bf05282dc4423f08e7cf29d78a7b08e6d349d6
jdmpro <58115966+jdmpro@users.noreply.github.com>
2024-05-05 04:12:57 -0700

    Update WaybarCava.sh

--- config/hypr/scripts/WaybarCava.sh
+++ config/hypr/scripts/WaybarCava.sh
@@ -19,7 +19,7 @@ done
 config_file="/tmp/bar_cava_config"
 cat >"$config_file" <<EOF
 [general]
-bars = 10
+bars = 8
 
 [input]
 method = pulse
@@ -33,7 +33,7 @@ ascii_max_range = 7
 EOF
 
 # Kill cava if it's already running
-pkill -f "cava -p $config_file"
+#pkill -f "cava -p $config_file"
 
 # Read stdout from cava and perform substitution in a single sed command
 cava -p "$config_file" | sed -u "$dict"
